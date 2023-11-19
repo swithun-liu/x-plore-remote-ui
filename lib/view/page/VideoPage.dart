60102be7708947b0c2bfa6f929c1ac157f9945fe
swithun <2571021108@qq.com>
2023-11-19 17:56:59 +0800

    kaishi video

---
+++ lib/view/page/VideoPage.dart
@@ -0,0 +1,16 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class VideoPage extends StatefulWidget {
+  const VideoPage({super.key});
+
+  @override
+  State<VideoPage> createState() => _VideoPageState();
+}
+
+class _VideoPageState extends State<VideoPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
